b023e4a5d8ec5b9836c1c868e860229d89495da7
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2021-09-23 10:53:37 +0530

    Add files via upload

---
+++ Practical2/Practical2_Task1/Practical1.cpp
@@ -0,0 +1,7 @@
+//If we don't use iosttream header file but use cout function
+using namespace std;
+int main()
+{
+	cout<<"Hello World";
+	return 0;
+}
---
+++ Practical2/Practical2_Task2/Practical2.cpp
@@ -0,0 +1,8 @@
+//If we use .h with iostream the how  pre processor will react 
+#include<iostream.h>
+using namespace std;
+int main()
+{
+	cout<<"Hello World";
+	return 0;
+}
